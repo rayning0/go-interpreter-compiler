e8f762e8dbbb3c01e933d6e69ad14f0521d5f1ad
Raymond Gan <rayning@yahoo.com>
2020-02-13 21:22:51 -0800

    Add new 1 character tokens

--- src/monkey/lexer/lexer_test.go
+++ src/monkey/lexer/lexer_test.go
@@ -8,14 +8,17 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `let five = 5;
-let ten = 10;
+	input :=
+		`let five = 5;
+		let ten = 10;
 
-let add = fn(x, y) {
-	x + y;
-};
+		let add = fn(x, y) {
+			x + y;
+		};
 
-let result = add(five, ten);`
+		let result = add(five, ten);
+		!-/*5;
+		5 < 10 > 5;`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -56,6 +59,18 @@ let result = add(five, ten);`
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
