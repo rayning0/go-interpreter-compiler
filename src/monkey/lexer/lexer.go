485287c675b68d8e89458138bc324c756e833079
Raymond Gan <rayning@yahoo.com>
2020-02-13 19:02:21 -0800

    Finish rough draft of Lexer: Turns string of 6 lines of code into tokens
    - In Lexer, handle letters, numbers, and illegal chars
    - readIdentifier(): loops through letters till hit non-letter, then return substring.
    - isLetter(): Boolean. Treats '_' as letter.
    - isDigit(): Boolean
    - skipWhitespace(): loops through all non-letters/numbers
    - readNumber(): like readIdentifier() but for digits
    - Add LookupIdent(), which returns TokenType if identifier or keyword
    - Change lexer_test

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -40,6 +40,9 @@ func (l *Lexer) readChar() {
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -60,12 +63,67 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+
+			//The early exit here, our return tok statement, is necessary because when
+			//calling readIdentifier(), we call readChar() repeatedly and advance our
+			//readPosition and position fields past the last character of the current
+			//identifier. So we don’t need the call to readChar() after the switch
+			//statement again.
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
+
 	l.readChar()
 
 	return tok
 }
 
+// If current character is a letter, read rest of the identifier/keyword
+// till we hit a non-letter. Having read that identifier/keyword, find out if
+// it's an identifier or keyword, to use correct token.TokenType.
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+// treat "_" as letter, so identifiers/keywords may have it, like "foo_bar"
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -12,7 +12,7 @@ const (
 	EOF     = "EOF"     // parser stops here
 
 	// Identifiers + literals
-	IDENT = "IDENT" // variable names
+	IDENT = "IDENT" // "identifier" = variable name
 	INT   = "INT"
 
 	// Operators
@@ -31,3 +31,16 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
