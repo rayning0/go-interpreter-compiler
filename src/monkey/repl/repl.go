eb90934048e7197826dc5d9d64e7b5875c363c8a
Raymond Gan <rayning@yahoo.com>
2020-02-13 23:47:37 -0800

    Add REPL that tokenizes what user types and prints token.Token struct for each

---
+++ src/monkey/repl/repl.go
@@ -0,0 +1,32 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">> "
+
+// Read from the input source until encountering a \n,
+// take the just read line and pass it to an instance of our
+// lexer and finally print all the tokens the lexer gives us
+// until we encounter EOF.
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok) // print struct w/ field names
+		}
+	}
+}
